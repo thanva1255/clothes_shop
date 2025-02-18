2550e18eb68d7e6231d777fd79da67003091d0c9
Thanva <thanva1255@gmail.com>
2025-02-18 17:25:08 +0700

    added UI flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get "products/index"
+  get "welcome/index"
+  get "collections/index"
+
+  resources :products
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -10,5 +14,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "products#index"
+  root "welcome#index"
 end
