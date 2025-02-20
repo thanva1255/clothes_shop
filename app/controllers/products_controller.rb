17a1a4424ef5aa641ae897c3339c8fad66e9126c
Thanva <thanva1255@gmail.com>
2025-02-20 15:08:19 +0700

    implemented filtering product by category on products index page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,28 @@
 class ProductsController < ApplicationController
+  before_action :update_category_as_selected, only: [ :index ]
   def index
-    @products = Product.all
+    @categories = Category.all
   end
 
   def show
     @product = Product.find(params[:id])
   end
+
+  private
+    def update_category_as_selected
+      category_id = params[:category]
+
+      if category_id != nil
+        selected_category = Category.find(category_id)
+        @products = selected_category.products
+        @selected_category_name = selected_category.name
+        @selected_category_id = selected_category.id
+        @is_show_all_category = false
+      else
+        @products = Product.all
+        @selected_category_name = "Products"
+        @selected_category_id = nil
+        @is_show_all_category = true
+      end
+    end
 end
--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,2 +1,8 @@
 module ProductsHelper
+  def selected_category?(selected_category, category_id)
+    return false if selected_category == nil
+    return false if selected_category != category_id
+
+    true
+  end
 end
