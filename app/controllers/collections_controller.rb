ef5123d5fb78685faf6113ac834dfdd59bd529a8
Thanva <thanva1255@gmail.com>
2025-03-25 14:56:13 +0700

    implemented collection new page

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,4 +1,10 @@
 class CollectionsController < ApplicationController
+  def initialize
+    super
+
+    @collection_domain = Cloth::Collection.new
+  end
+
   def index
     @collections = Collection.all
   end
@@ -8,4 +14,16 @@ class CollectionsController < ApplicationController
   end
 
   def new; end
+
+  def create
+    @collection_domain.create_collection(
+      name: params[:name],
+      description: params[:description]
+    )
+
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to collections_path }
+    end
+  end
 end
